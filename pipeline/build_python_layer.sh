94ee4b0ebc52635eafb8946e64ff5c829fac525b
Glynn Bird <glynn.bird@gmail.com>
2021-01-04 14:13:34 +0000

    fixed linting errors on snapshot.py

--- pipeline/build_python_layer.sh
+++ pipeline/build_python_layer.sh
@@ -5,7 +5,8 @@ python -m venv choirless
 source choirless/bin/activate
 # install choirless_lib from git (and its dependencies) into the python directory
 pip3 install "git+https://github.com/Choirless/renderer@e0f2c46d5e4b2a34fa67b034f141fb286cffab2a#egg=choirless_lib&subdirectory=python/choirless_lib" -t python
-pip3 install ffmpeg -t ./python
+# https://github.com/kkroening/ffmpeg-python
+pip3 install ffmpeg-python -t ./python
 #pip install pandas -t ./python
 deactivate
 zip -r python.zip python/
--- pipeline/snapshot.py
+++ pipeline/snapshot.py
@@ -1,21 +1,23 @@
 from pathlib import Path
 from functools import partial
+import os
+
+import boto3
+
 import ffmpeg
 
-import boto3 
-
-
 def main(event, context):
+    '''Converts a video file into a jpg snapshot'''
 
-    key = event.Records[0].s3.object.key
+    key = event['Records'][0]['s3']['object']['key']
     choir_id, song_id, part_id = Path(key).stem.split('.')[0].split('+')
-    bucket = event.Records[0].s3.bucket.name
+    bucket = event['Records'][0]['s3']['bucket']['name']
     dst_bucket = os.environ['DEST_BUCKET']
+    # geo = event['Records'][0]['awsRegion']
 
     if key.endswith(".jpg"):
         return {}
 
-    geo = event.Records[0].awsRegion
     # Get the service client.
     s3 = boto3.client('s3')
 
@@ -29,11 +31,12 @@ def main(event, context):
     )
 
     # Generate the URL to get 'key-name' from 'bucket-name'
+    keyjpg = key + '.jpg'
     puturl = s3.generate_presigned_url(
       ClientMethod='put_object',
         Params={
           'Bucket': dst_bucket,
-          'Key': key + '.jpg'
+          'Key': keyjpg
         }
     )
 
@@ -45,10 +48,10 @@ def main(event, context):
                         method='PUT',
                         seekable=0,
                         vframes=1)
-    stdout, stderr = out.run()
+    #stdout, stderr = out.run()
+    out.run()
 
-    ret = {"status": "ok",
-           "snapshot_key": key + '.jpg',
+    ret = {"snapshot_key": keyjpg,
            "choir_id": choir_id,
            "song_id": song_id,
            "part_id": part_id,
