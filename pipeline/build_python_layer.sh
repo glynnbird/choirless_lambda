a425b6adc3f76d648cb51bf0ac00e8454e92ffd4
Glynn Bird <glynn.bird@gmail.com>
2021-01-04 15:19:55 +0000

    removed choirless_lib - not needed - we can use boto3 instead

--- pipeline/build_python_layer.sh
+++ pipeline/build_python_layer.sh
@@ -4,7 +4,7 @@ mkdir python
 python -m venv choirless
 source choirless/bin/activate
 # install choirless_lib from git (and its dependencies) into the python directory
-pip3 install "git+https://github.com/Choirless/renderer@e0f2c46d5e4b2a34fa67b034f141fb286cffab2a#egg=choirless_lib&subdirectory=python/choirless_lib" -t python
+# pip3 install "git+https://github.com/Choirless/renderer@e0f2c46d5e4b2a34fa67b034f141fb286cffab2a#egg=choirless_lib&subdirectory=python/choirless_lib" -t python
 # https://github.com/kkroening/ffmpeg-python
 pip3 install ffmpeg-python -t ./python
 #pip install pandas -t ./python
--- pipeline/choirless_lib/choirless_lib/__init__.py
+++
@@ -1,3 +0,0 @@
-from .signed_urls import create_signed_url
-from .mqtt_status import mqtt_status
-from .cos_client import create_cos_client
