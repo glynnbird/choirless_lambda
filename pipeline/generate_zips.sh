3fd73db630dc96298dfd0ceb37e0fd353aa4c16a
Glynn Bird <glynn.bird@gmail.com>
2021-01-06 11:01:07 +0000

    generate_zips.sh script

---
+++ pipeline/generate_zips.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for filename in *.py; do
+  # if this is not a .test.js files
+  STUB=`echo $filename | sed 's/.py//g'`
+  echo "$STUB"
+  rm "${STUB}.zip"
+  zip -r "${STUB}.zip" "${STUB}.py" lib/ 
+done
